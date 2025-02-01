afcbb16af62ebc179bbcb1528f21fc6855a40b10
RemiGirard <remi.girard@protonmail.com>
2025-02-01 22:41:59 +0100

    init

---
+++ dataviz/empreinte-souffrance/src/Presentation/components/Production.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+export default () => {
+  return (<div className={`w-full h-full flex flex-col justify-center items-center`}>Production</div>);
+};
---
+++ dataviz/empreinte-souffrance/src/app/production/page.tsx
@@ -0,0 +1,5 @@
+import Production from "@/Presentation/components/Production";
+
+export default () => {
+  return (<Production />);
+};
